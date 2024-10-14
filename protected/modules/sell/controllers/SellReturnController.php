b86c63393dc1dc7f76301fee18f765ee1a1bf610
raihanuddin <raihanptl21@gmail.com>
2024-10-14 17:38:27 +0600

    Refactor SellReturnDetails model and update attribute name for replace_model_id to Replace Product

--- protected/modules/sell/controllers/SellReturnController.php
+++ protected/modules/sell/controllers/SellReturnController.php
@@ -48,6 +48,78 @@ class SellReturnController extends RController
 	}
 
 	/**
+	 * Create a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreateProductReturn(){
+		$model=new SellReturn();
+        $model2 = new SellReturnDetails();
+
+		// Uncomment the following line if AJAX validation is needed
+		$this->performAjaxValidation($model);
+		if (Yii::app()->request->isAjaxRequest) {
+			// db transaction
+			$transaction = Yii::app()->db->beginTransaction();
+			if(isset($_POST['SellReturn']))
+			{
+				try{
+					$model->attributes=$_POST['SellReturn'];
+					$model->customer_id = $_POST['SellReturn']['customer_id'];
+					$model->return_amount = 0;
+					$model->discount = 0;
+					$model->discount_percentage = 0;
+					$model->costing = 0;
+					$model->return_type = SellReturn::DAMAGE_RETURN;
+					$model->remarks = $_POST['SellReturn']['remarks'];
+					$model->status = SellReturn::RETURN_STATUS_PENDING;
+					$model->is_deleted = 0;
+					if($model->save()){
+						$detailsData = $_POST['SellReturnDetails']['model_id'];
+						
+						foreach ($detailsData as $key =>  $detail){
+							// die(var_dump($details));
+							// die(var_dump($_POST['SellReturnDetails']['replace_product_sl_no'][$key]));
+							$detailModel = new SellReturnDetails();
+							$detailModel->return_id = $model->id;
+							$detailModel->model_id = $_POST['SellReturnDetails']['model_id'][$key];
+							$detailModel->return_qty = $_POST['SellReturnDetails']['qty'][$key];
+							$detailModel->sell_price = 0;
+							$detailModel->purchase_price = 0;
+							$detailModel->product_sl_no = $_POST['SellReturnDetails']['product_sl_no'][$key];
+							$detailModel->row_total = 0;
+							$detailModel->costing = 0;
+							$detailModel->discount_amount = 0;
+							$detailModel->discount_percentage = 0;
+							// $detailModel->replace_model_id = $_POST['SellReturnDetails']['replace_model_id'][$key];
+							// $detailModel->replace_product_sl_no = $_POST['SellReturnDetails']['replace_product_sl_no'][$key];
+							$detailModel->created_by = Yii::app()->user->id;
+							$detailModel->created_at = date('Y-m-d H:i:s');
+							$detailModel->is_deleted = 0;
+							if(!$detailModel->save()){
+								throw new Exception('Product return details creation failed');
+							}
+						}
+						$transaction->commit();
+						echo CJSON::encode(array(
+							'status' => 'success',
+							// 'soReportInfo' => $this->renderPartial('voucherPreview', array('data' => $data, 'new' => true), true, true), //
+						));
+					}
+				}catch(Exception $e){
+					$transaction->rollback();
+					echo json_encode(['status' => 'error', 'message' => 'Product return creation failed']);
+				}
+			}
+		}
+
+        $this->pageTitle = "PRODUCT RETURN CREATE";
+        $this->render('_formProductReturn', array(
+			'model'=>$model,
+            'model2' => $model2,
+		));
+	}
+
+	/**
 	 * Updates a particular model.
 	 * If update is successful, the browser will be redirected to the 'view' page.
 	 * @param integer $id the ID of the model to be updated
