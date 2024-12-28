544054fd28fa284113b3a9d9030d7abf2330e553
Md. Raihan Uddin <raihanuddin2@yahoo.com>
2024-12-28 13:48:47 +0600

    Add export to Excel functionality for price list view

--- themes/erp/views/report/priceListView.php
+++ themes/erp/views/report/priceListView.php
@@ -135,3 +135,25 @@ echo "</div>";
         text-align: left;
     }
 </style>
+
+<script>
+    $(function () {
+        $(".exportToExcel").click(function (e) {
+            var table = $('.table2excel');
+
+            if (table && table.length) {
+                var preserveColors = (table.hasClass('table2excel_with_colors') ? true : false);
+                $(table).table2excel({
+                    exclude: ".noExl",
+                    name: "Excel Document Name",
+                    filename: "PRICE_LIST-" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+                    fileext: ".xls",
+                    exclude_img: true,
+                    exclude_links: true,
+                    exclude_inputs: true,
+                    preserveColors: preserveColors
+                });
+            }
+        });
+    });
+</script>
