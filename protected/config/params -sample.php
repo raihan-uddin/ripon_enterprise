cdaba4890bb06247308df4e6fd3826964e281dec
Md. Raihan Uddin <blackedition.me@gmail.com>
2025-10-23 20:43:35 +0600

    param update

--- protected/config/params -sample.php
+++ protected/config/params -sample.php
@@ -4,8 +4,21 @@ return array(
     'servername' => 'RAIHAN UDDIN',
     'serverhost' => 'RAIHAN UDDIN',
     'adminEmail' => 'blackedition.me@gmail.com',
+    'adminPhone' => '+880 1680-52 7922',
+    'dateFormat' =>'Y-m-d',
+    'timeZone' => 'Asia/Dhaka',
     'copyrightBy' => date('y'),
     'version' => 1.1,
     'developedBy' => 'RAIHAN UDDIN',
     'developedByUrl' => 'https://raihan-6d1dc.firebaseapp.com/',
+    'company' => [
+        'name' => 'THE MIHAN ENGINEERS',
+        'address_line_1' => '583 Anamika Concorde 1st floor Shop-202,',
+        'address_line_2' => 'Begum Rokeya Soroni, Mirpur',
+        'phone_1' => '+880 1609-254 301',
+        'phone_2' => '',
+        'email_1' => 'info@tmebd.com',
+        'email_2' => '',
+        'web' => 'www.tmebd.com',
+    ]
 );
