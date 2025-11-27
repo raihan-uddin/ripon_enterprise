00fd7a8ddfb452293275153385c4b8cdca689653
Md. Raihan Uddin <blackedition.me@gmail.com>
2025-11-27 09:49:58 +0600

    new reports

--- themes/erp/views/site/report-shortcut.php
+++ themes/erp/views/site/report-shortcut.php
@@ -18,18 +18,74 @@
                     <?php
                     if ($stockReportPermission) {
                         ?>
-                        <a class="dropdown-item" href="<?= Yii::app()->createUrl("inventory/inventory/stockReport") ?>">Stock
-                            Report</a>
-                        <a class="dropdown-item" href="<?= Yii::app()->createUrl("inventory/inventory/stockReportSupplierWise") ?>">Stock
-                            Report (Supplier Wise)</a>
+                        <a class="dropdown-item" href="<?= Yii::app()->createUrl("inventory/inventory/stockReport") ?>">
+                            <i class="fa fa-archive mr-2 text-primary"></i> Stock Report
+                        </a>
+                        <a class="dropdown-item" href="<?= Yii::app()->createUrl("inventory/inventory/stockReportSupplierWise") ?>">
+                            <i class="fa fa-user-secret mr-2 text-success"></i> Stock Report (Supplier Wise)
+                        </a>
                         <?php
                     }
                     ?>
                     <a class="dropdown-item"
-                       href="<?= Yii::app()->createUrl("report/PriceListView") ?>">Price List</a>
-                    <a class="dropdown-item disabled"
-                       href="<?= Yii::app()->createUrl("inventory/inventory/stockReport") ?>">Slow
-                        Moving Report</a>
+                       href="<?= Yii::app()->createUrl("report/PriceListView") ?>">
+                       <i class="fa fa-tag mr-2 text-info"></i> Price List
+                    </a>
+
+                    <div class="dropdown-divider"></div>
+
+                    <!-- NEW REPORTS -->
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-bolt mr-2 text-warning"></i> Fast Moving Report
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-ban mr-2 text-danger"></i> Non-Moving / Dead Stock
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-hourglass-half mr-2 text-orange"></i> Near Expiry Stock
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-bars mr-2 text-secondary"></i> Batch Wise Stock
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-balance-scale mr-2 text-dark"></i> Stock Valuation
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-level-down mr-2 text-success"></i> Reorder Level Report
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-archive mr-2 text-primary"></i> Overstock Report
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-calendar mr-2 text-info"></i> Inventory Aging Report
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-pencil-square-o mr-2 text-purple"></i> Stock Adjustment Report
+                    </a>
+
+                    <a class="dropdown-item" href="#">
+                        <i class="fa fa-list-alt mr-2 text-secondary"></i> Item Stock Ledger
+                    </a>
+
+                    <div class="dropdown-divider"></div>
+
+                    <!-- DISABLED FUTURE FEATURES -->
+                    <a class="dropdown-item disabled" href="#">
+                        <i class="fa fa-line-chart mr-2 text-muted"></i> AI Safety Stock Forecast (Coming Soon)
+                    </a>
+
+                    <a class="dropdown-item disabled" href="#">
+                        <i class="fa fa-gears mr-2 text-muted"></i> AI Inventory Optimization (Coming Soon)
+                    </a>
+
                 </div>
             </div>
             <?php
